3936c0625347fcebb2578eba93357e512f88cbbe
aaizuss <aaizuss@gmail.com>
2017-02-10 15:27:18 -0600

    Implement location service, protocols, and a mock + tests for LocationServices

---
+++ app/Graffiti/Graffiti/LocationService.swift
@@ -0,0 +1,80 @@
+//
+//  LocationService.swift
+//  Graffiti
+//
+//  Created by Amanda Aizuss on 2/9/17.
+//  Copyright © 2017 Amanda Aizuss. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+
+/* Since multiple view controllers need to be CLLocationManagerDelegates, we made this class
+ * to avoid duplicating code
+ * This class receives the updates from the CLLocationManager and will update the currentLocation property
+ * To use: make a variable like locationService = LocationService.sharedInstance
+ * then, in viewDidLoad, call locationService.startUpdatingLocation() to start generating updates
+ * Call locationService.stopUpdatingLocation() to stop generating updates (don't know why we'd do this)
+ */
+
+class LocationService: NSObject, CLLocationManagerDelegate {
+    
+    // classes share LocationService and do not create separate instances (singleton)
+    // reference: https://krakendev.io/blog/the-right-way-to-write-a-singleton
+    static let sharedInstance = LocationService()
+    var locationManager : LocationManagerProtocol
+    var currentLocation: CLLocation?
+    let distanceFilter: CLLocationDistance = 100 // min distance (meters) device must move horizontally for update event to be generated
+    
+    public init(givenLocationManager: LocationManagerProtocol? = nil) {
+        if let unwrappedLocationManager = givenLocationManager {
+            self.locationManager = unwrappedLocationManager
+        }   else {
+            self.locationManager = CLLocationManager()
+        }
+        super.init()
+        
+        locationManager.requestAuthorizationWhenNotDetermined()
+        
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.distanceFilter = distanceFilter
+        locationManager.delegate = self
+    }
+    
+    func getLatitude() -> CLLocationDegrees? {
+        return currentLocation?.coordinate.latitude
+    }
+    
+    func getLongitude() -> CLLocationDegrees? {
+        return currentLocation?.coordinate.longitude
+    }
+
+    
+    func startUpdatingLocation() {
+        print("Starting location updates")
+        locationManager.startUpdatingLocation()
+    }
+    
+    func stopUpdatingLocation() {
+        print("Stopping location updates")
+        locationManager.stopUpdatingLocation()
+    }
+    
+    // MARK: LocationManagerDelegate functions
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedAlways {
+            locationManager.startMonitoringSignificantLocationChanges()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        // get location
+        guard let location = locations.last else {
+            return
+        }
+        self.currentLocation = location
+    }
+
+}
+
---
+++ app/Graffiti/GraffitiTests/MockLocationManager.swift
@@ -0,0 +1,39 @@
+//
+//  MockLocationManager.swift
+//  Graffiti
+//
+//  Created by Amanda Aizuss on 2/10/17.
+//  Copyright © 2017 Amanda Aizuss. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+
+class MockLocationManager: LocationManagerProtocol {
+    var desiredAccuracy = kCLLocationAccuracyBest
+    var distanceFilter: CLLocationDistance = 100
+    var delegate: CLLocationManagerDelegate?
+    var authorized = false
+    
+    func requestAuthorizationWhenNotDetermined() {
+        authorized = true
+    }
+    
+    func startUpdatingLocation() {
+        
+    }
+    
+    func stopUpdatingLocation() {
+    
+    }
+    
+    func startMonitoringSignificantLocationChanges() {
+    
+    }
+    
+    func setLocation(location: CLLocation) {
+        delegate?.locationManager!(CLLocationManager(), didUpdateLocations: [location])
+    }
+    
+    
+}
