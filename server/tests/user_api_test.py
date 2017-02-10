2b07dad3a17a107f318020068e85e024e6f5fe70
Ron Yehoshua <ronyehoshua@gmail.com>
2017-02-10 13:41:44 -0600

    finished backend api http request handling tests

--- server/tests/user_api_test.py
+++ server/tests/user_api_test.py
@@ -52,7 +52,7 @@ class UserTestCase(APITestCase):
     def test_get_existent_user(self):
         # creates a user with userId: 1
         self.create_user()
-        rv = self.app.get('/users/1',
+        rv = self.app.get('/users/userid=1',
                 headers=dict(
                     idToken=9402234123712),
                 follow_redirects=True)
@@ -64,7 +64,7 @@ class UserTestCase(APITestCase):
         self.check_user_fields(data, 1, '', '', '', '')
 
     def test_get_nonexistent_user_by_user_id(self):
-        rv = self.app.get('/users/1',
+        rv = self.app.get('/users/userid=1',
                 headers=dict(
                     idToken=9402234123712),
                 follow_redirects=True)
@@ -75,7 +75,7 @@ class UserTestCase(APITestCase):
         assert data['error'] == "User not found."
 
     def test_update_nonexistent_user(self):
-        rv = self.app.put('/users/1',
+        rv = self.app.put('/users/userid=1',
                 headers=dict(
                     idToken=9402234123712),
                 follow_redirects=True)
@@ -89,7 +89,7 @@ class UserTestCase(APITestCase):
         # creates a user with userId: 1
         self.create_user()
 
-        rv = self.app.put('/users/1',
+        rv = self.app.put('/users/userid=1',
                 data=dict(
                     userid=1,
                     username='l33t',
