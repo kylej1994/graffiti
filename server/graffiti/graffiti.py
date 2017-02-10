afcda78315d08b2f9ec8d1bc641d8a51ebaa6952
Ron Yehoshua <ronyehoshua@gmail.com>
2017-02-09 18:39:22 -0600

    took out unnecessary graffiti.py work for dev merge

--- server/graffiti/graffiti.py
+++ server/graffiti/graffiti.py
@@ -7,7 +7,3 @@ app = Flask(__name__)
 def init_db():
     """ Initializes the database. """
     pass
-
-@app.route('/posts/create', methods=['POST'])
-def create_post():
-    return json.dumps({'lat': 'post created'})
