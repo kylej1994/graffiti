4900452df8b2a295b9ed8e5aad9cc5d196ede014
Ron Yehoshua <ronyehoshua@gmail.com>
2017-02-10 12:22:25 -0600

    finished user api http request tests

--- server/tests/post_api_test.py
+++ server/tests/post_api_test.py
@@ -15,5 +15,33 @@ class PostTestCase(APITestCase):
     def tearDown(self):
         super(APITestCase, self).tearDown()
 
+    def check_post_fields(self,
+        data, post_id, location, created_at, poster_id, num_votes)
+
+    """ Post related API calls. """    
+    def test_empty_db(self):
+        post_text = 'omg first graffiti post'
+        lat = 29.12123
+        lon = 32.12943
+        created_at = 0.1
+        poster_username = 'jeffdean'
+        rv = self.app.post('/posts/create',
+                data=dict(
+                    latitude = lat,
+                    longitude = lon,
+                    created_at = created_at,
+                    poster = poster_username),
+                headers=dict(
+                    idToken=9402234123712),
+            follow_redirects=True)
+        assert rv.status_code == 200
+
+        data = json.loads(rv.data)
+        assert data['lat'] == lat
+        assert data['lon'] == lon
+        assert data['created_at'] == 0.1
+        assert data['poster'] == poster_username
+
+
 if __name__ == '__main__':
     unittest.main()
