e263c25553d1b03e71134ee8c40b7f2c73c36250
Will Longendyke <wlongendyke@vileplume.cs.uchicago.edu>
2017-02-14 21:51:16 -0600

    adding some initial stuff for the post view controller. Does it work? No idea.

--- app/Graffiti/Graffiti/TextPostViewController.swift
+++ app/Graffiti/Graffiti/TextPostViewController.swift
@@ -8,11 +8,17 @@
 
 import UIKit
 
-class TextPostViewController: UIViewController {
+class TextPostViewController: UIViewController, UITextFieldDelegate {
 
+    // MARK: Properties
+    @IBOutlet weak var postTextField: UITextField!
+    @IBOutlet weak var postButton: UIButton!
+    @IBOutlet weak var charCount: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        self.postTextField.delegate = self
         // Do any additional setup after loading the view.
     }
 
@@ -21,15 +27,32 @@ class TextPostViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func textViewDidChange(_ textView: UITextField) {
+        
+        let textViewFixedWidth: CGFloat = self.postTextField.frame.size.width
+        let newSize: CGSize = self.postTextField.sizeThatFits(CGSize(textViewFixedWidth, CGFloat(MAXFLOAT)))
+        var newFrame: CGRect = self.postTextField.frame
+        
+        //var textViewYPosition = self.postTextField.frame.origin.y
+        let heightDifference = self.postTextField.frame.height - newSize.height
+        
+        if (abs(heightDifference) > 20) {
+            newFrame.size = CGSize(fmax(newSize.width, textViewFixedWidth), newSize.height)
+            newFrame.offsetBy(dx: 0.0, dy: 0)
+        }
+        self.postTextField.frame = newFrame
+        
     }
-    */
-
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        let newLength = (textField.text?.characters.count)! + string.characters.count - range.length
+        
+        //change the value of the label
+        charCount.text =  String(newLength)
+        
+        //return true to allow the change, if you want to limit the number of characters in the text field use something like
+        return newLength <= 140
+    }
+    
 }
