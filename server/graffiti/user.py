00e1685108052f25d0ba81acbad4e25d9136c0d8
emmaasmith <akari24@gmail.com>
2017-02-15 21:58:14 -0600

    I really hope I'm only committing user and userpost

--- server/graffiti/user.py
+++ server/graffiti/user.py
@@ -1,5 +1,6 @@
 import json
 import sys
+import re
 sys.path.append('..')
 from graffiti import db
 from sqlalchemy import Column, Float, Integer, String
@@ -12,7 +13,7 @@ import geoalchemy2
 class User(db.Model):
     __tablename__ = 'user'
 
-    # Only alnum or _ in username. <25 chars. Will be enforced in validation.
+    # Only alnum or _ in username. Between 3 and 25 chars inclusive
     user_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     google_aud = db.Column(db.String(100), unique=True)
     username = db.Column(db.String(100), unique=True)
@@ -20,34 +21,126 @@ class User(db.Model):
     phone_number = db.Column(db.String(100), unique=True)
     name = db.Column(db.String(100))
     email = db.Column(db.String(100), unique=True)
-    text_tag = db.Column(db.String(160))
+    bio = db.Column(db.String(160))
     join_timestamp = db.Column(db.DateTime)
     has_been_suspended = db.Column(db.Boolean)
 
-    def __init__(self, username, google_aud, phone_number, name, email, text_tag):
+    def __init__(self, username, google_aud, phone_number, name, email, bio):
         self.username = ""
         self.phone_number = ""
         # We have not yet implemented the parameter validation methods 
-        if (self.validate_username(username)):
-            self.username = username
+        set_username(username)
         if (self.validate_phone_number(phone_number)):
             self.phone_number = phone_number
 
-        self.google_aud = google_aud
+        set_google_aud(google_aud)
         self.name = name
         self.email = email
-        self.text_tag = text_tag
+        self.bio = bio
         self.join_timestamp = datetime.fromtimestamp(time()).isoformat()
         self.has_been_suspended = False
 
     def __repr__(self):
         return '<username {}>'.format(self.username)
 
+    # Only alnum or _ in username. Between 3 and 25 chars inclusive.
     def validate_username(self, username):
+        if (len(username) > 25 || len(username) < 3):
+            return False
+        if (re.match('^\w+$', username)):
+            return False
+        return True
+
+    # Only alpha. Fewer than or equal to 50 characters.
+    def validate_name(self, name):
+        if (len(name) > 50):
+            return False
+        if (!name.isalpha()):
+            return False
         return True
 
     def validate_phone_number(self, phone_number):
-        return True
+        new_number = re.sub('-', '', phone_number)
+        if (len(new_number) == 11 && new_number[0] == '1'):
+            new_number = new_number[1:]
+        if (!new_number.isdigit() || len(new_number) != 10):
+            return None
+
+        return new_number
+
+    def get_user_id(self):
+        return user_id
+
+    def get_google_aud(self):
+        return google_aud
+
+    def get_username(self):
+        return username
+
+    def get_phone_number(self):
+        return phone_number
+
+    def get_name(self):
+        return name
+
+    def get_email(self):
+        return email
+
+    def get_bio(self):
+        return bio
+
+    def get_join_timestamp(self):
+        return self.bio
+
+    def get_has_been_suspended(self):
+        return self.has_been_suspended
+
+    # No validations implemented
+    def set_google_aud(self):
+        self.google_aud = google_aud
+        db.session.commit()
+
+    # Only alnum or _ in username. Between 3 and 25 chars inclusive.
+    def set_username(self, username):
+        if (self.validate_username(username)):
+            self.username = username
+            db.session.commit()
+            return True
+        else
+            return False
+
+    # Updates by removing country code 1, remove dashes
+    def set_phone_number(self, phone_number):
+        new_number = self.validate_phone_number(phone_number)
+        if (new_number != None):
+            self.phone_number = new_number
+            db.session.commit()
+            return True
+        else
+            return False
+
+    # Only letters
+    def set_name(self, name):
+        if (validate_name(name)):
+            self.name = name
+            db.session.commit()
+            return True
+        else
+            return False
+
+    # No validations implemented
+    def set_bio(self, bio):
+        self.bio = bio
+        db.session.commit()
+
+    # Only bool
+    def set_has_been_suspended(self, suspended):
+        if (type(suspended) == types.BooleanType)):
+            self.has_been_suspended = suspended
+            db.session.commit()
+            return True
+        else:
+            return False
 
     def to_json_fields_for_FE(self):
         return json.dumps(dict(
@@ -55,4 +148,4 @@ class User(db.Model):
             username=self.username,
             name=self.name,
             email=self.email,
-            textTag=self.text_tag))
+            textTag=self.bio))
