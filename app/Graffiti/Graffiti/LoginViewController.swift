804085002fab966ad28a9e2cc2e74dacfd0798a1
aaizuss <aaizuss@gmail.com>
2017-02-17 16:58:42 -0600

    Remove custom google login button, use storyboard for background color and logo

--- app/Graffiti/Graffiti/LoginViewController.swift
+++ app/Graffiti/Graffiti/LoginViewController.swift
@@ -10,56 +10,30 @@ import UIKit
 
 class LoginViewController: UIViewController, GIDSignInUIDelegate, UITextFieldDelegate {
     
-    var btnSignIn : UIButton!
+    //var btnSignIn : UIButton!
+    var btnSignIn : GIDSignInButton!
     var label : UILabel!
-    var welcome : UILabel!
     var loginerror : UILabel!
     var untoolong : UILabel!
     var logo : UIImageView!
     @IBOutlet var btnNewsFeed: UIButton!
     @IBOutlet weak var usertextnew: UITextField!
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
-         self.view.backgroundColor = UIColor.black
+        self.view.backgroundColor = UIColor(red:0.30, green:0.82, blue:0.88, alpha:1.0)
         
         GIDSignIn.sharedInstance().uiDelegate = self
         self.usertextnew.delegate = self
-    
-        
-        
-        logo = UIImageView(frame: CGRect(0, 0, 150, 150))
-        logo.center = CGPoint(view.center.x, 150)
-        logo.image = UIImage(named:"Logo")
-        view.addSubview(logo)
-        
-        let btnSize : CGFloat = 100
-        btnSignIn = UIButton(frame: CGRect(0,0,btnSize,btnSize))
+
+        btnSignIn = GIDSignInButton(frame: CGRect(0,0,230,48))
         btnSignIn.center = view.center
-        btnSignIn.setImage(UIImage(named: "GoogleLogo"), for: UIControlState.normal)
-        btnSignIn.addTarget(self, action: #selector(btnSignInPressed(_:)), for: UIControlEvents.touchUpInside)
-        
-        //Circular button
-        btnSignIn.layer.cornerRadius = btnSize/8
-        btnSignIn.layer.masksToBounds = true
-        btnSignIn.layer.borderColor = UIColor.black.cgColor
-        btnSignIn.layer.borderWidth = 2
+        btnSignIn.style = GIDSignInButtonStyle.wide
         view.addSubview(btnSignIn)
 
-        
-        welcome = UILabel(frame: CGRect(100,300,600,100))
-        welcome.text = "Welcome to Grafitti"
-        welcome.font = welcome.font.withSize(40)
-        welcome.center = CGPoint(view.center.x, 50)
-        welcome.textColor = UIColor.cyan
-        welcome.textAlignment = NSTextAlignment.center
-        view.addSubview(welcome)
-        
         loginerror = UILabel(frame: CGRect(0,0,200,100))
-        loginerror.text = "There Was an Error Connectting to Account, Idtoken from Google is Missing"
+        loginerror.text = "There Was an Error Connecting to Account, Idtoken from Google is Missing"
         loginerror.numberOfLines = 4 //Multi-lines
         loginerror.font = loginerror.font.withSize(10)
         loginerror.center = CGPoint(view.center.x, 250)
@@ -81,12 +55,11 @@ class LoginViewController: UIViewController, GIDSignInUIDelegate, UITextFieldDel
         untoolong.isHidden = true
         
         
-        
         label = UILabel(frame: CGRect(0,0,200,100))
         label.center = CGPoint(view.center.x, 300)
         label.numberOfLines = 0 //Multi-lines
         label.text = "Please Sign in to Graffiti Using Google"
-        label.textColor = UIColor.cyan
+        label.textColor = UIColor.white
         label.textAlignment = NSTextAlignment.center
         view.addSubview(label)
         
@@ -94,16 +67,11 @@ class LoginViewController: UIViewController, GIDSignInUIDelegate, UITextFieldDel
         name: NSNotification.Name(rawValue: "ToggleAuthUINotification"),object: nil)
         
         toggleAuthUI()
-        
-
 }
-    
-    
     
     private func textViewDidBeginEditing(_ textView: UITextView) {
         usertextnew.text = ""
         usertextnew.textColor = UIColor .black
-        
     }
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
@@ -112,34 +80,21 @@ class LoginViewController: UIViewController, GIDSignInUIDelegate, UITextFieldDel
         return true
     }
     
-    override func viewDidAppear(_ animated: Bool) {
-        // Show keyboard by default
-        super.viewDidLoad()
-        usertextnew.becomeFirstResponder()
-    }
-
-    
-
-    func btnSignInPressed(_ sender: UIButton) {
-        GIDSignIn.sharedInstance().signIn()
-    }
-    
-    
+//    override func viewDidAppear(_ animated: Bool) {
+//        // Show keyboard by default
+//        super.viewDidLoad()
+//        usertextnew.becomeFirstResponder()
+//    }
 
     func newuser(newuser: Dictionary<String, Any>) {
         let nu = newuser["new_user"]
         if (nu as? Bool == false) {
             print("current user already there")
         usertextnew.isHidden = true
-         self.welcome.isHidden = true
           self.btnNewsFeed.isHidden = false
-            
-            
-        }
-        else {
+        } else {
         print ("new user")
         self.btnNewsFeed.isHidden = true
-        self.welcome.isHidden = false
         untoolong.text = "You Must Enter a Username"
         untoolong.isHidden = false
         let user = newuser["user"] as! User
@@ -163,7 +118,6 @@ class LoginViewController: UIViewController, GIDSignInUIDelegate, UITextFieldDel
                 case.success:
                     self.usertextnew.isHidden = true
                     self.untoolong.isHidden = true
-                    self.welcome.isHidden = true
                     self.btnNewsFeed.isHidden = false
                     print("why")
                     
@@ -172,9 +126,7 @@ class LoginViewController: UIViewController, GIDSignInUIDelegate, UITextFieldDel
                     self.untoolong.isHidden = false
                 }
             }
-            
-    }
-        
+        }
     
     }
     
@@ -182,13 +134,10 @@ class LoginViewController: UIViewController, GIDSignInUIDelegate, UITextFieldDel
         loginerror.isHidden = false
     }
     
-    
   
-    
     func toggleAuthUI() {
         if (GIDSignIn.sharedInstance().hasAuthInKeychain()){
             // Signed in
-            welcome.isHidden = true
             btnNewsFeed.isHidden = true
             btnSignIn.isHidden = true
             loginerror.isHidden = true
@@ -204,7 +153,6 @@ class LoginViewController: UIViewController, GIDSignInUIDelegate, UITextFieldDel
         } else {
             
             // Not Signed In
-            welcome.isHidden = false
             btnNewsFeed.isHidden = true
             btnSignIn.isHidden = false
             loginerror.isHidden = true
@@ -214,9 +162,7 @@ class LoginViewController: UIViewController, GIDSignInUIDelegate, UITextFieldDel
     }
     
     deinit {
-        NotificationCenter.default.removeObserver(self,
-                                                            name: NSNotification.Name(rawValue: "ToggleAuthUINotification"),
-                                                            object: nil)
+        NotificationCenter.default.removeObserver(self, name: NSNotification.Name(rawValue: "ToggleAuthUINotification"), object: nil)
     }
     
     @objc func receiveToggleAuthUINotification(_ notification: NSNotification) {
@@ -228,10 +174,6 @@ class LoginViewController: UIViewController, GIDSignInUIDelegate, UITextFieldDel
                 self.label.text = userInfo["statusText"]!
             }
         }
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
     }
     
 }
