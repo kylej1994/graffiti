22df54736f813cdb67ca78925e6b762a7edb1ccc
Will Longendyke <wlongendyke@blastoise.cs.uchicago.edu>
2017-02-16 18:41:50 -0600

    initial commit for the user profile

--- app/Graffiti/Graffiti/ProfileViewController.swift
+++ app/Graffiti/Graffiti/ProfileViewController.swift
@@ -7,8 +7,13 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ProfileViewController: UIViewController {
+class ProfileViewController: UITableViewController {
+    
+    let api = API.sharedInstance
+    let locationManager = LocationService.sharedInstance
+    var posts: [Post] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,15 +26,62 @@ class ProfileViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func getPostsByUser() {
+        // make network request
+        
+        /*
+        api.getPosts(userid: currUserID) { response in
+            switch response.result {
+            case .success:
+                print("hello i am in success")
+                if let json = response.result.value as? [String:Any],
+                    let posts = json["posts"] as? [Post] {
+                    print(posts)
+                    self.posts = posts
+                }
+            case .failure(let error):
+                print(error)
+            }
+        }
+         */
     }
-    */
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let numRows = posts.count
+        if numRows == 0 {
+            //setupEmptyBackgroundView()
+            tableView.separatorStyle = .none
+            tableView.backgroundView?.isHidden = false
+        } else {
+            tableView.separatorStyle = .singleLine
+            tableView.backgroundView?.isHidden = true
+        }
+        return numRows
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellIdentifier = "FeedCell"
+        
+        // downcast cell to the custom cell class
+        // guard safely unwraps the optional
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? FeedTableViewTextCell else {
+            fatalError("The dequeue cell is not an instance of FeedTableViewTextCell.")
+        }
+        
+        // this is where we get the posts from the post model
+        let post = posts[indexPath.row]
+        cell.textView.text = post.getText()
+        cell.votesLabel.text = "\(post.getRating())" // this might be a bad practice'
+        
+        //cell.dateLabel.text = post.getTimeAdded()
+        
+        cell.voteTapAction = { (cell) in
+            print("upvote tapped!")
+            print(tableView.indexPath(for: cell)!.row)
+        }
+        return cell
+    }
 
 }
