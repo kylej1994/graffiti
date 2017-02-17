28b050b91a8d3aacc7052a5f611018d75709e704
klu1622 <kathleenlu.26@gmail.com>
2017-02-17 13:56:30 -0600

    fixed post tests to fit new implementation

--- server/tests/post_test.py
+++ server/tests/post_test.py
@@ -4,14 +4,18 @@ import unittest
 
 from flask_api_test import APITestCase
 sys.path.append('..')
-from graffiti import graffiti, db
-from graffiti.models import Post #this doesnt exist yet
+from graffiti import graffiti, db, post
+from post import Post
+
 from datetime import datetime, date
 import time
-
 import geoalchemy
 
 class PostTestCase():
+
+    user1 = User("username", 
+            "1008719970978-hb24n2dstb40o45d4feuo2ukqmcc6381.apps.googleusercontent.com",
+            "9172825753", "name", "email", "bio")
 
     def setUp(self):
         self.db_fd, graffiti.app.config['DATABASE'] = tempfile.mkstemp()
@@ -19,7 +23,7 @@ class PostTestCase():
 
         self.app = graffiti.app.test_client()
         db.create_all()
-        db.session.add(Post("text", 51.5192028, -0.140863, "username"))
+        db.session.add(Post("text", 51.5192028, -0.140863, user1.get_user_id()))
         db.session.flush()
 
         with graffiti.app.app_context():
@@ -31,32 +35,27 @@ class PostTestCase():
         db.session.remove()
         db.drop_all()
 
-    def test_increment_votes(self):
+    # tests that the poster id (user_id) of the post is being set correctly
+    def test_get_poster_id(self):
+        post = db.query(Post).filter(Post.poster_username=="username").first()
+        self.assertTrue(post.get_poster_id() == user1.get_user_id())
+        self.assertFalse(post.get_poster_id() == user1.get_user_id() + 1)
+
+    def test_set_votes(self):
         post = db.query(Post).filter(Post.poster_username=="username").first()
         self.assertTrue(post.num_votes == 0)
-        post.increment_votes()
+        post.set_votes(1)
         self.assertTrue(db.query(Post).filter(Post.poster_username=="username").first().num_votes == 1)
-        post.increment_votes()
-        self.assertTrue(db.query(Post).filter(Post.poster_username=="username").first().num_votes == 2)
-
-    def test_decrement_votes(self):
-        post = db.query(Post).filter(Post.poster_username=="username").first()
-        post.increment_votes()
-        post.increment_votes()
-        post.decrement_votes()
-        self.assertTrue(db.query(Post).filter(Post.poster_username=="username").first().num_votes == 1)
-        post.decrement_votes()
+        post.set_votes(-1)
         self.assertTrue(db.query(Post).filter(Post.poster_username=="username").first().num_votes == 0)
-        post.decrement_votes()
-        self.assertTrue(db.query(Post).filter(Post.poster_username=="username").first().num_votes == -1)
 
     def test_get_created_at(self):
         before = time.time()
-        new_post = Post("text2", 51.5192028, -0.140863, "username2")
+        new_post = Post("text2", 51.5192028, -0.140863, user1.get_user_id())
         db.session.add(new_post)
         db.session.flush()
         after = time.time()
-        middle = time.mktime(db.query(Post).filter(Post.poster_username=="username2").first().created_at.timetuple())
+        middle = time.mktime(db.query(Post).filter(Post.poster_username=="username").first().created_at.timetuple())
         assertTrue(before <= middle <= after)
 
     def test_get_text(self):
@@ -66,23 +65,12 @@ class PostTestCase():
         self.assertTrue(text == "text")
         self.assertFalse(text == "username")
 
-    def test_get_poster_username(self):
-        post = db.query(Post).filter(Post.poster_username=="username").first()
-        username = post.test_get_poster_username()
-        self.assertIsNotNone(username)
-        self.assertTrue(username == "username")
-        self.assertFalse(username == "text")
-
-    #change the function def of delete_post() to return bool
     def test_delete_post(self):
-        post2 = Post("text2", -37.8167, 144.9667, "username2")
-        assertFalse(post2.delete_post())
         post = db.query(Post).filter(Post.poster_username=="username").first()
-        assertTrue(post.delete_post())
+        post.delete_post()
         assertIsNone(db.query(Post).filter(Post.poster_username=="username").first())
 
-
-    def test_get_nearby_posts(self):
+    def test_find_posts_within_loc(self):
         longitude = 51.5192028
         latitude = -0.140863
         hackney_longitude = 51.5457865
@@ -90,8 +78,10 @@ class PostTestCase():
         aus_longitude = -37.8167
         aus_langititude = 144.9667
 
-        db.session.add(Post("in australia", -37.8167, 144.9667, "username2"))
-        db.session.add(Post("in hackney", 51.5457865, -0.0554184, "username2"))
+        to_add = Post("in australia", -37.8167, 144.9667, user1.get_user_id())
+        to_add2 = Post("in hackney", 51.5457865, -0.0554184, user1.get_user_id())
+        to_add.save_post()
+        to_add2.save_post()
 
         distance1 = 1
         distance2 = 10000
@@ -100,13 +90,13 @@ class PostTestCase():
         post = db.query(Post).filter(Post.poster_username=="username").first()
 
         #gets all post within distance 1 
-        posts = post.get_nearby_posts(distance1)
+        posts = Post.find_posts_within_loc(longitutde, latittude, distance1)
         assertTrue(posts.size() == 1)
         assertTrue(posts[0].longitude == post.longitude)
         assertTrue(posts[0].latitude == post.latitude)
 
         #gets all two posts within distance 2, should get 2 posts in the UK
-        posts = post.get_nearby_posts(distance2)
+        posts = Post.find_posts_within_loc(longitutde, latittude, distance2)
         assertTrue(posts.size() == 2)
         assertTrue(posts[0].longitude == longitude)
         assertTrue(posts[0].latitude == latitude)
@@ -114,7 +104,7 @@ class PostTestCase():
         assertTrue(posts[1].latitude == hackney_latitude)
    		
    		#gets all three posts within distance 3
-        posts = post.get_nearby_posts(distance3)
+        posts = Post.find_posts_within_loc(longitutde, latittude, distance3)
         assertTrue(posts.size() == 3)
         assertTrue(posts[0].longitude == longitude)
         assertTrue(posts[0].latitude == latitude)
