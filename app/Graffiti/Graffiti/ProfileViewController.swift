a48329da822272238e0c5b56571dc41a0e3ddaaf
Will Longendyke <wlongendyke@blastoise.cs.uchicago.edu>
2017-02-16 23:22:15 -0600

    gotta merge with judah's extra stuff

--- app/Graffiti/Graffiti/ProfileViewController.swift
+++ app/Graffiti/Graffiti/ProfileViewController.swift
@@ -24,6 +24,10 @@ class ProfileViewController: UIViewController, UITableViewDelegate, UITableViewD
     var profilePicture:UIImage? = nil
     var userName:String? = nil
     
+    var btnDisconnect : UIButton!
+    var label : UILabel!
+    var btnSignOut: UIButton!
+    
     
     @IBOutlet var tableView: UITableView!
     
@@ -36,6 +40,34 @@ class ProfileViewController: UIViewController, UITableViewDelegate, UITableViewD
         tableView.contentInset.top = 20
         
         getPostsByUser()
+        
+        btnSignOut = UIButton(frame: CGRect(0,0,100,30))
+        btnSignOut.center = CGPoint(view.center.x, 500)
+        btnSignOut.setTitle("Sign Out", for: UIControlState.normal)
+        btnSignOut.setTitleColor(UIColor.blue, for: UIControlState.normal)
+        btnSignOut.setTitleColor(UIColor.cyan, for: UIControlState.highlighted)
+        btnSignOut.addTarget(self, action: #selector(btnSignOutPressed(_:)), for: UIControlEvents.touchUpInside)
+        view.addSubview(btnSignOut)
+        
+        btnDisconnect = UIButton(frame: CGRect(0,0,100,30))
+        btnDisconnect.center = CGPoint(view.center.x, 200)
+        btnDisconnect.setTitle("Disconnect", for: UIControlState.normal)
+        btnDisconnect.setTitleColor(UIColor.blue, for: UIControlState.normal)
+        btnDisconnect.setTitleColor(UIColor.cyan, for: UIControlState.highlighted)
+        btnDisconnect.addTarget(self, action: #selector(btnDisconnectPressed(_:)), for: UIControlEvents.touchUpInside)
+        view.addSubview(btnDisconnect)
+        
+        label = UILabel(frame: CGRect(0,0,200,100))
+        label.center = CGPoint(view.center.x, 300)
+        label.numberOfLines = 0 //Multi-lines
+        label.text = "Please Sign in to Graffiti Using Google"
+        label.textColor = UIColor.cyan
+        label.textAlignment = NSTextAlignment.center
+        view.addSubview(label)
+        
+        label.isHidden = true
+        btnDisconnect.isHidden = true
+
     }
     
     func getPostsByUser() {
@@ -101,6 +133,29 @@ class ProfileViewController: UIViewController, UITableViewDelegate, UITableViewD
             return cell
         }
     }
+    
+    func btnDisconnectPressed(_ sender: UIButton) {
+        self.navigationController?.popToRootViewController(animated: true)
+        
+        let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        let nextviewController = storyBoard.instantiateViewController(withIdentifier: "LoginViewController") as! LoginViewController
+        self.present(nextviewController, animated: true, completion: nil)
+        
+        
+        label.text = "Signed out."
+        label.isHidden = false
+        LoginViewController().toggleAuthUI()
+        
+        
+        
+        
+    }
+    
+    func btnSignOutPressed(_ sender: UIButton) {
+        GIDSignIn.sharedInstance().disconnect()
+        label.text = "Disconnecting."
+    }
+
     
     // method to run when table view cell is tapped
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
