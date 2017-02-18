fbb34f4d3e96e17d9518da407e6802312f35d475
klu1622 <kathleenlu.26@gmail.com>
2017-02-17 23:45:34 -0600

    more working

--- server/tests/post_test.py
+++ server/tests/post_test.py
@@ -4,71 +4,61 @@ import unittest
 
 from flask_api_test import APITestCase
 sys.path.append('..')
-from graffiti import graffiti, post, user
+from graffiti import graffiti
 from graffiti.user import User
+from graffiti.post import Post
 from graffiti.graffiti import db
 
 from datetime import datetime, date
 import time
 
-class PostTestCase():
-
-    user1 = User("username", 
+user1 = User("username", 
             "1008719970978-hb24n2dstb40o45d4feuo2ukqmcc6381.apps.googleusercontent.com",
             "9172825753", "name", "email", "bio")
+test_post = Post("text", 51.5192028, -0.140863, user1.get_user_id())
+poster_id = test_post.get_poster_id()
+
+class PostTestCase(APITestCase):
 
     def setUp(self):
-        self.db_fd, graffiti.app.config['DATABASE'] = tempfile.mkstemp()
-        graffiti.app.config['TESTING'] = True
-
+        super(APITestCase, self).setUp()
         self.app = graffiti.app.test_client()
-        db.create_all()
-        db.session.add(Post("text", 51.5192028, -0.140863, user1.get_user_id()))
-        db.session.flush()
-
-        with graffiti.app.app_context():
-            # initializes the database that is used
-            graffiti.init_db()
+        db.session.add(test_post)
+        db.session.commit()
 
     def tearDown(self):
-        super(APITestCase, self).tearDown()
         db.session.remove()
         db.drop_all()
 
     # tests that the poster id (user_id) of the post is being set correctly
     def test_get_poster_id(self):
-        post = db.query(Post).filter(Post.poster_username=="username").first()
-        assert (post.get_poster_id() == user1.get_user_id())
-        assert (post.get_poster_id() != user1.get_user_id() + 1)
+        post = db.session.query(Post).filter(Post.poster_id==poster_id).first()
+        self.assertTrue(post.get_poster_id() == poster_id)
+        self.assertFalse(post.get_poster_id() == poster_id + 100)
 
     def test_set_votes(self):
-        post = db.query(Post).filter(Post.poster_username=="username").first()
+        post = db.session.query(Post).filter(Post.poster_id==poster_id).first()
         self.assertTrue(post.num_votes == 0)
         post.set_votes(1)
-        self.assertTrue(db.query(Post).filter(Post.poster_username=="username").first().num_votes == 1)
+        self.assertTrue(db.session.query(Post).filter(Post.poster_id==poster_id).first().num_votes == 1)
         post.set_votes(-1)
-        self.assertTrue(db.query(Post).filter(Post.poster_username=="username").first().num_votes == 0)
+        self.assertTrue(db.session.query(Post).filter(Post.poster_id==poster_id).first().num_votes == 0)
 
     def test_get_created_at(self):
         before = time.time()
         new_post = Post("text2", 51.5192028, -0.140863, user1.get_user_id())
         db.session.add(new_post)
-        db.session.flush()
+        db.session.commit()
         after = time.time()
-        middle = time.mktime(db.query(Post).filter(Post.poster_username=="username").first().created_at.timetuple())
-        assertTrue(before <= middle <= after)
+        middle = time.mktime(db.session.query(Post).filter(Post.poster_id==poster_id).first().created_at.timetuple())
+        self.assertTrue(before <= middle <= after)
 
     def test_get_text(self):
-        post = db.query(Post).filter(Post.poster_username=="username").first()
+        post = db.session.query(Post).filter(Post.poster_id==poster_id).first()
         text = post.get_text()
         self.assertIsNotNone(text)
         self.assertTrue(text == "text")
         self.assertFalse(text == "username")
-
-    def test_delete_post(self):
-        post = db.query(Post).filter(Post.poster_username=="username").first()
-        post.delete_post()
-        assertIsNone(db.query(Post).filter(Post.poster_username=="username").first())
 
     def test_find_posts_within_loc(self):
         longitude = 51.5192028
@@ -87,7 +77,7 @@ class PostTestCase():
         distance2 = 10000
         distance3 = 16900 * 1000
 
-        post = db.query(Post).filter(Post.poster_username=="username").first()
+        post = db.session.query(Post).filter(Post.poster_id==poster_id).first()
 
         #gets all post within distance 1 
         posts = Post.find_posts_within_loc(longitutde, latittude, distance1)
@@ -114,7 +104,13 @@ class PostTestCase():
         assertTrue(posts[2].latitude == aus_latitude)
 
 
-
+    def test_delete_post(self):
+        to_delete = Post("text2", 51.5192024, -0.140862, user1.get_user_id())
+        db.session.add(to_delete)
+        db.session.commit()
+        post_id = to_delete.post_id
+        to_delete.delete_post()
+        self.assertIsNone(db.session.query(Post).filter(Post.post_id==post_id).first())
 
 if __name__ == '__main__':
     unittest.main()
