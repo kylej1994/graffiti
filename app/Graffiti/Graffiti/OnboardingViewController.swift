cc2620e88c8afc338171ebf513151fc5012b3d46
Henry <hlewis@uchicago.edu>
2017-02-21 13:22:17 -0600

    add new onboarding view

---
+++ app/Graffiti/Graffiti/OnboardingViewController.swift
@@ -0,0 +1,13 @@
+//
+//  OnboardingViewController.swift
+//  Graffiti
+//
+//  Created by Henry Lewis on 2/21/17.
+//  Copyright © 2017 Amanda Aizuss. All rights reserved.
+//
+
+import Foundation
+
+class OnboardingViewController: UIViewController {
+    
+}
