aa8f3267ac92064dc276d149775cc9c8528ea1f2
Henry <hlewis@uchicago.edu>
2017-02-24 17:09:24 -0600

    user json tests

--- app/Graffiti/GraffitiTests/UserTests.swift
+++ app/Graffiti/GraffitiTests/UserTests.swift
@@ -39,4 +39,37 @@ class UserTests: XCTestCase {
             XCTFail("Wrong Error!")
         }
     }
+    
+    func testToJSON() {
+        let user = User(id: 1, username: "username", name: "First Last", email: "me@email.com", phoneNumber: "123-456-7890", bio: "This is a bio")
+        
+        let json: [String: Any] = user.toJSON()
+        XCTAssertEqual(json["userid"] as? Int, 1)
+        XCTAssertEqual(json["username"] as? String, "username")
+        XCTAssertEqual(json["name"] as? String, "First Last")
+        XCTAssertEqual(json["email"] as? String, "me@email.com")
+        XCTAssertEqual(json["bio"] as? String, "This is a bio")
+        XCTAssertEqual(json["phone_number"] as? String, "123-456-7890")
+    }
+    
+    func testFromJSON() {
+        let json: [String: Any] = [
+            "userid": 1,
+            "username": "username",
+            "name": "First Last",
+            "email": "me@email.com",
+            "bio": "This is a bio",
+            "phone_number": "123-456-7890"
+        ]
+        
+        let user = User(JSON: json)
+        XCTAssertNotNil(user)
+        XCTAssertEqual(user?.getId(), 1)
+        XCTAssertEqual(user?.getUsername(), "username")
+        XCTAssertEqual(user?.getName(), "First Last")
+        XCTAssertEqual(user?.getEmail(), "me@email.com")
+        XCTAssertEqual(user?.getBio(), "This is a bio")
+        XCTAssertEqual(user?.getPhoneNumber(), "123-456-7890")
+    }
+
 }
