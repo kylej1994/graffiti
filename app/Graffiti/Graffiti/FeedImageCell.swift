2ca6c9dbd460d673e5a930b32046ed7ec9c29ef5
aaizuss <aaizuss@gmail.com>
2017-02-26 12:19:53 -0600

    add image and text cells that inherit from feed cell

---
+++ app/Graffiti/Graffiti/FeedImageCell.swift
@@ -0,0 +1,24 @@
+//
+//  FeedImageCell.swift
+//  Graffiti
+//
+//  Created by Amanda Aizuss on 2/26/17.
+//  Copyright © 2017 Amanda Aizuss. All rights reserved.
+//
+
+import UIKit
+
+class FeedImageCell: FeedTableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ app/Graffiti/Graffiti/FeedTextCell.swift
@@ -0,0 +1,24 @@
+//
+//  FeedTextCell.swift
+//  Graffiti
+//
+//  Created by Amanda Aizuss on 2/26/17.
+//  Copyright © 2017 Amanda Aizuss. All rights reserved.
+//
+
+import UIKit
+
+class FeedTextCell: FeedTableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
