edebf763df65fddaaaacd56e44c6678fa65f89a2
Henry <hlewis@uchicago.edu>
2017-03-03 17:46:21 -0600

    add map controller

---
+++ app/Graffiti/Graffiti/MapViewController.swift
@@ -0,0 +1,23 @@
+//
+//  MapViewController.swift
+//  Graffiti
+//
+//  Created by Henry Lewis on 3/3/17.
+//  Copyright © 2017 Amanda Aizuss. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+    @IBOutlet weak var map: MKMapView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+    }
+
+}
