691cf0cd006b58304b2647aadeedb36776bf404a
Ron Yehoshua <ronyehoshua@gmail.com>
2017-03-03 11:23:22 -0600

    comment in textseg stuff

--- server/graffiti/post_api.py
+++ server/graffiti/post_api.py
@@ -1,6 +1,6 @@
 import datetime
 import json
-#import textseg
+import textseg
 
 from flask import Blueprint, request
 from post import Post
@@ -34,8 +34,7 @@ def validate_vote(vote):
 
 def validate_text(text):
 	# Correctly count grapheme clusters
-	#return len(textseg.GCStr(text)) <= 140
-	return True
+	return len(textseg.GCStr(text)) <= 140
 
 def generate_error_response(message, code):
 	error_response = {}
