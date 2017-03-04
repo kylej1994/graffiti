bd5486e3a3f073aef58ce04263c6ff8b7dde335e
klu1622 <kathleenlu.26@gmail.com>
2017-03-03 22:16:50 -0600

    uncommented tests whoops

--- server/tests/run_all_tests.sh
+++ server/tests/run_all_tests.sh
@@ -3,9 +3,9 @@
 # tells the server to use the local database, not the production one
 export GRAFFITI_DB=postgresql://localhost/mydb
 
-# python auth_api_test.py
-# python user_api_test.py
+python auth_api_test.py
+python user_api_test.py
 python post_api_test.py
-# python post_test.py
-# python user_test.py
-# python userpost_test.py
+python post_test.py
+python user_test.py
+python userpost_test.py
